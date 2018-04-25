d2ea8d8c5e882675726518239ae32fb9e358cb52
WinChua <winchua@foxmail.com>
2018-04-25 13:25:36 +0800

    add the memory image of a process 'cat /proc/<pid>/maps' which will output the VMAs of the process.

---
+++ Link_Load_Library/Chap06/maps.sh
@@ -0,0 +1,11 @@
+cat /proc/6931/maps
+'
+00400000-004c0000 r-xp 00000000 00:1d 10432955                           /data/home/caiyong/git/RecordOfBooks/Link_Load_Library/Chap06/SectionMapping.elf
+006bf000-006c2000 rw-p 000bf000 00:1d 10432955                           /data/home/caiyong/git/RecordOfBooks/Link_Load_Library/Chap06/SectionMapping.elf
+006c2000-006c5000 rw-p 00000000 00:00 0 
+01d07000-01d2a000 rw-p 00000000 00:00 0                                  [heap]
+7ffef8864000-7ffef8885000 rw-p 00000000 00:00 0                          [stack]
+7ffef8900000-7ffef8902000 r--p 00000000 00:00 0                          [vvar]
+7ffef8902000-7ffef8904000 r-xp 00000000 00:00 0                          [vdso]
+ffffffffff600000-ffffffffff601000 r-xp 00000000 00:00 0                  [vsyscall]
+'
